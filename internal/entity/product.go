46ec42270591a56090dfcb80c77204a4f502efb5
Shandler Menezes Gama <shandler.gama@tj.ac.gov.br>
2023-09-21 14:11:24 -0500

    Commit Entity User-Produtos e pesquisa de User

---
+++ internal/entity/product.go
@@ -0,0 +1,56 @@
+package entity
+
+import (
+	"errors"
+	"time"
+
+	"github.com/shandler/go-expert-inicio-projeto/pkg/entity"
+)
+
+var (
+	ErrIDIsRequired    = errors.New("id is required")
+	ErrInvalidID       = errors.New("invalid id")
+	ErrNameIsRequired  = errors.New("name is required")
+	ErrPriceIsRequired = errors.New("price is required")
+	ErrInvalidPrice    = errors.New("invalid price")
+)
+
+type Product struct {
+	ID       entity.ID `json:"id"`
+	Name     string    `json:"name"`
+	Price    int       `json:"price"`
+	CreateAt time.Time `json:"create_at"`
+}
+
+func NewProduct(name string, price int) (*Product, error) {
+	product := &Product{
+		ID:       entity.NewID(),
+		Name:     name,
+		Price:    price,
+		CreateAt: time.Now(),
+	}
+	err := product.Validate()
+	if err != nil {
+		return nil, err
+	}
+	return product, nil
+}
+
+func (p *Product) Validate() error {
+	if p.ID.String() == "" {
+		return ErrIDIsRequired
+	}
+	if _, err := entity.ParseID(p.ID.String()); err != nil {
+		return ErrInvalidID
+	}
+	if p.Name == "" {
+		return ErrNameIsRequired
+	}
+	if p.Price == 0 {
+		return ErrPriceIsRequired
+	}
+	if p.Price < 0 {
+		return ErrInvalidPrice
+	}
+	return nil
+}
